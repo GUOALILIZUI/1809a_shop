67f99b76a649d7b754ad410bae076a925a79cccb
alili <2115108140@qq.com>
2019-04-08 01:42:58 +0000

    fff

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                   hellow 1809a
+                   HELLOW ALILI
                 </div>
 
                 <div class="links">
