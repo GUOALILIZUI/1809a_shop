e3c3ad73868e41faabdf944e79bc508b9ad4cf02
alili <2115108140@qq.com>
2019-04-09 14:47:28 +0800

    sss

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -12,7 +12,13 @@ class IndexController extends Controller
 				
    //接口配置
     public function check(Request $request){
- 	   echo $request->input('echostr');
+	    //首次接入检测请求是否为微信
+	    echo $request->input('echostr');
+	    $content=file_get_contents("php://input");
+	    $time=date('Y-m-d H:i:s');
+	    $str=$time.$content."\n";
+	    file_put_contents("logs/check.log",$str,FILE_APPEND);
+	    echo 'success';
     }
 	
 	
