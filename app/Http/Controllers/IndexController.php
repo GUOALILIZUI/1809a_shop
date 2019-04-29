52ff467289faa66cef682d7b4bb94634597eb15f
alili <2115108140@qq.com>
2019-04-29 08:19:20 +0000

    商品

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -281,7 +281,7 @@ class IndexController extends Controller
         $url="https://api.weixin.qq.com/sns/oauth2/access_token?appid=$appId&secret=$secret&code=$code&grant_type=authorization_code";
         $info=file_get_contents($url);
         $info2=json_decode($info);
-        $openID=$info['openid'];
+        $openID=$info2['openid'];
         $access=$this->accessToken();
         $urll="https://api.weixin.qq.com/cgi-bin/user/info?access_token=$access&openid=$openID&lang=zh_CN";
         $objJson=file_get_contents($urll);
